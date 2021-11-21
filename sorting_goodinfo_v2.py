ca2df2eb724274c53fb591bcbe157a9b9ba281e6
raywu <raywu@aaeon.com.tw>
2021-11-22 00:43:47 +0800

    GoodInfo_v2 - 2021.11.19 尚未完成

--- sorting_goodinfo_v2.py
+++ sorting_goodinfo_v2.py
@@ -1,32 +1,39 @@
 import pandas as pd
 
+filter = 'Filter_20211119.xlsx'
+input = 'GoodInfo_StockList_20211119.csv'
+reference = 'GoodInfo_StockList_20211118.xlsx'
+output = 'GoodInfo_StockList_20211119.xlsx'
+
 
 def stock_id_transfer(transfer):
     result = str(transfer['代號']).replace('=', '')
     return result.replace('"', '')
 
 
-df = pd.read_csv('GoodInfo_StockList_20211118.csv')
+df = pd.read_csv(input)
 
 df['代號'] = df.apply(stock_id_transfer, axis=1)
-filter_df = pd.read_excel('Filter_20211117.xlsx')
+filter_df = pd.read_excel(filter)
 merged_df = df.merge(filter_df, how='outer',
                      indicator=True, on=['代號', '名稱']).loc[lambda x: x['_merge'] == 'left_only']
-merged_df.drop(columns={'_merge'}, inplace=True)
-merged_df.to_excel('temp_GoodInfo_StockList_20211118.xlsx')
+#merged_df.drop(columns={'_merge'}, inplace=True)
+merged_df = merged_df[['代號', '名稱', 'Class']]
+merged_df.to_excel(output)
+
+df = pd.read_excel(output)
+filter_df = pd.read_excel(reference)
+merged_df = df.merge(filter_df, how='outer', on=['代號', '名稱'],
+                     indicator=True).loc[lambda x: x['_merge'] != 'right_only']
+merged_df.rename(inplace=True, columns={'_merge': 'merge_status'})
+#merged_df.drop(columns={'_merge'}, inplace=True)
+merged_df.to_excel(output)
 
 
-df = pd.read_excel('temp_GoodInfo_StockList_20211118.xlsx')
-filter_df = pd.read_excel('GoodInfo_StockList_20211117_.xlsx')
-merged_df = df.merge(filter_df, how='outer',
-                     indicator=True, on=['名稱'])
-merged_df.drop(columns={'_merge'}, inplace=True)
-merged_df.to_excel('temp_GoodInfo_StockList_20211118.xlsx')
-
-
-df = pd.read_excel('temp_GoodInfo_StockList_20211118.xlsx')
-filter_df = pd.read_excel('Filter_20211118.xlsx')
-merged_df = df.merge(filter_df, how='outer',
-                     indicator=True, on=['名稱']).loc[lambda x: x['_merge'] == 'left_only']
-merged_df.drop(columns={'_merge'}, inplace=True)
-merged_df.to_excel('temp_GoodInfo_StockList_20211118.xlsx')
+#df = pd.read_excel('temp_GoodInfo_StockList_20211118.xlsx')
+#filter_df = pd.read_excel('Filter_20211118.xlsx')
+# merged_df = df.merge(filter_df, how='outer',
+#                     indicator=True, on=['名稱']).loc[lambda x: x['_merge'] == 'left_only']
+#merged_df.drop(columns={'_merge'}, inplace=True)
+# merged_df.to_excel('temp_GoodInfo_StockList_20211118.xlsx')
+#
