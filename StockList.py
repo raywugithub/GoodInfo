7a4369886a6b731a41adadad42b22d4567a58e5c
raywu <raywu@aaeon.com.tw>
2021-12-22 10:28:02 +0800

    GoodInfo_v2 - 2021-12-21 未完成

--- StockList.py
+++ StockList.py
@@ -1,8 +1,10 @@
 from numpy import result_type
 import pandas as pd
 
-today = '2021-12-16'
-yesterday = '2021-12-15'
+account_cty = 0
+
+today = '2021-12-21'
+yesterday = '2021-12-16'
 
 
 def is_stair(stair):
@@ -27,10 +29,12 @@ stock_list_df['代號'] = stock_list_df.apply(
     lambda x: str(x['代號']).replace('=', ''), axis=1)
 stock_list_df['代號'] = stock_list_df.apply(
     lambda x: str(x['代號']).replace('"', ''), axis=1)
-stock_list_df['股票名稱_國泰'] = stock_list_df.apply(
-    lambda x: x['名稱'] + '(' + str(x['代號']) + ')', axis=1)
+if account_cty == 1:
+    stock_list_df['股票名稱_國泰'] = stock_list_df.apply(
+        lambda x: x['名稱'] + '(' + str(x['代號']) + ')', axis=1)
 stock_list_df['股票名稱_群益'] = stock_list_df.apply(
     lambda x: str(x['代號']) + x['名稱'], axis=1)
+stock_list_df['股票名稱_群益'] = stock_list_df['股票名稱_群益'].astype('str')
 # stock_list_df.to_excel('temp.xlsx', index=False)
 
 stock_list_yesterday_df = pd.read_excel('StockList_' + yesterday + '.xlsx')
@@ -44,24 +48,26 @@ merge_df['類型'+'_' + today] = merge_df.apply(copy_type_N_from_yesterday, axis
 merge_df.to_excel('StockList_' + today + '.xlsx', index=False)
 stock_list_df = pd.read_excel('StockList_' + today + '.xlsx')
 
-# 未平倉
-open_position_df = pd.read_excel('TodayOpenPosition_' + today + '.xlsx')
-open_position_df.rename(
-    columns={'股票名稱': '股票名稱_國泰', '\t庫存股數': '庫存股數_國泰', '\t持有成本': '持有成本_國泰', '\t未實現損益': '未實現損益_國泰'}, inplace=True)
-open_position_df = open_position_df[[
-    '股票名稱_國泰', '庫存股數_國泰', '持有成本_國泰', '未實現損益_國泰']]
-merge_df = stock_list_df.merge(
-    open_position_df, how='outer', on='股票名稱_國泰', indicator=True)
-merge_df.rename(
-    columns={'_merge': '_merge_with_OpenPosition_國泰'}, inplace=True)
-merge_df.to_excel('StockList_' + today + '.xlsx', index=False)
-stock_list_df = pd.read_excel('StockList_' + today + '.xlsx')
+if account_cty == 1:
+    # 未平倉
+    open_position_df = pd.read_excel('TodayOpenPosition_' + today + '.xlsx')
+    open_position_df.rename(
+        columns={'股票名稱': '股票名稱_國泰', '\t庫存股數': '庫存股數_國泰', '\t持有成本': '持有成本_國泰', '\t未實現損益': '未實現損益_國泰'}, inplace=True)
+    open_position_df = open_position_df[[
+        '股票名稱_國泰', '庫存股數_國泰', '持有成本_國泰', '未實現損益_國泰']]
+    merge_df = stock_list_df.merge(
+        open_position_df, how='outer', on='股票名稱_國泰', indicator=True)
+    merge_df.rename(
+        columns={'_merge': '_merge_with_OpenPosition_國泰'}, inplace=True)
+    merge_df.to_excel('StockList_' + today + '.xlsx', index=False)
+    stock_list_df = pd.read_excel('StockList_' + today + '.xlsx')
 
 # 未平倉
 open_position_df = pd.read_excel('TodayOpenPosition_' + today + '_.xlsx')
 open_position_df.rename(
     columns={'股票名稱': '股票名稱_群益', '庫存股數': '庫存股數_群益', '付出成本': '付出成本_群益', '損益': '損益_群益'}, inplace=True)
 open_position_df = open_position_df[['股票名稱_群益', '庫存股數_群益', '付出成本_群益', '損益_群益']]
+open_position_df['股票名稱_群益'] = open_position_df['股票名稱_群益'].astype('str')
 merge_df = stock_list_df.merge(
     open_position_df, how='outer', on='股票名稱_群益', indicator=True)
 merge_df.rename(
@@ -69,8 +75,10 @@ merge_df.rename(
 merge_df.to_excel('StockList_' + today + '.xlsx', index=False)
 stock_list_df = pd.read_excel('StockList_' + today + '.xlsx')
 
-stock_list_df['本週創新高'] = stock_list_df.apply(
+stock_list_df['5日創新高'] = stock_list_df.apply(
     lambda x: x['5日最高股價'] == x['一年最高股價'], axis=1)
+stock_list_df['10日創新高'] = stock_list_df.apply(
+    lambda x: x['10日最高股價'] == x['一年最高股價'], axis=1)
 stock_list_df['低於近5日低點'] = stock_list_df.apply(
     lambda x: x['成交'] <= x['5日最低股價'], axis=1)
 stock_list_df['低於近10日低點'] = stock_list_df.apply(
@@ -82,12 +90,21 @@ print_df = stock_list_df[stock_list_df['漲跌幅'] > 5]
 print(print_df[['代號', '名稱', '漲跌幅']])
 print('今日漲5%以上\n')
 
-open_position_cost = float(
-    stock_list_df['持有成本_國泰'].sum()) + float(stock_list_df['付出成本_群益'].sum())
+if account_cty == 1:
+    open_position_cost = float(
+        stock_list_df['持有成本_國泰'].sum()) + float(stock_list_df['付出成本_群益'].sum())
+else:
+    open_position_cost = float(stock_list_df['付出成本_群益'].sum())
 print('今日未平倉成本：  $', open_position_cost)
-open_position_profit = float(
-    stock_list_df['未實現損益_國泰'].sum()) + float(stock_list_df['損益_群益'].sum())
-open_position_profit_percent = round(
-    open_position_profit * 100 / open_position_cost, 2)
-print('今日未平倉績效：  $', open_position_profit,
-      ' ... ', open_position_profit_percent, '%')
+if account_cty == 1:
+    open_position_profit = float(
+        stock_list_df['未實現損益_國泰'].sum()) + float(stock_list_df['損益_群益'].sum())
+else:
+    open_position_profit = float(stock_list_df['損益_群益'].sum())
+try:
+    open_position_profit_percent = round(
+        open_position_profit * 100 / open_position_cost, 2)
+    print('今日未平倉績效：  $', open_position_profit,
+          ' ... ', open_position_profit_percent, '%')
+except:
+    print('今日未平倉績效：  N/A')
